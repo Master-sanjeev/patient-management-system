bf17ec69e3dca0f25e633fc561456dd7affd59b4
sanjeev sharma <sanjeev@sanjeevs-MacBook-Air.local>
2025-03-27 21:05:29 +0530

    Added necessary files to set up the project

---
+++ patient-management-service/src/test/java/com/sanjeev/learnings/springboot/patient/management/system/PatientManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjeev.learnings.springboot.patient.management.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
